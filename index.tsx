499fc3d72fba17fe9084735dcb863fbbdc5efebc
Padmini-ace <padhminiverma@gmail.com>
2026-01-11 19:11:34 +0530

    feat: complete professional overhaul of destill

--- index.tsx
+++ index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
+// We don't import index.css here anymore because we are using Tailwind CDN in HTML
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
